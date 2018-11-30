a393e1740f88900d87920691c234ff8473c67674
Slava <ukrslava@yandex.ua>
2018-11-30 13:45:07 +0200

    add StripFull

--- stripansi.go
+++ stripansi.go
@@ -2,12 +2,29 @@ package stripansi
 
 import (
 	"regexp"
+	"strings"
 )
 
 const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+const f = "[<>]"
 
 var re = regexp.MustCompile(ansi)
+var reF = regexp.MustCompile(f)
 
+// Strip удаляет ANSI символы
 func Strip(str string) string {
+	return re.ReplaceAllString(str, "")
+}
+
+// StripFullR также даляем символы которые не входят в GTK3 markup
+func StripFullR(str string) string {
+	str = reF.ReplaceAllString(str, "")
+	return re.ReplaceAllString(str, "")
+}
+
+// StripFull также даляем символы которые не входят в GTK3 markup
+func StripFull(str string) string {
+	str = strings.Replace(str, "<", "", -1)
+	str = strings.Replace(str, ">", "", -1)
 	return re.ReplaceAllString(str, "")
 }
